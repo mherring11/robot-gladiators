6e72807c6f0c812963c8c930513ef8592c11691b
mherring11 <m.herring11@yahoo.com>
2022-11-13 14:29:09 -0600

    add emeny people

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,24 @@
-// var playerName = 'Clank McKrank';
+// const playerName = 'Mike Herring';
 const playerName = window.prompt("What is your robot's name?");
 const playerHealth = 100;
 const playerAttack = 10;
 const playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
-const enemyName = "Roborto";
+const enemyNames = ["Roberto", "Amy Andriod", "Robo Trumble"];
 const enemyHealth = 50;
 const enemyAttack = 12;
 
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
+
+
 // fight function
-const fight = function() {
+const fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -68,4 +74,6 @@ const fight = function() {
 }; // end of fight function
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
